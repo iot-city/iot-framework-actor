76e0bcb9918cb6f3987e5de4348fb0c5c752d532
ardon <ardon@iot-city.org>
2021-07-02 04:49:42 +0800

    Modify the ApplicationConfigPool configure value.

--- iot-framework-actor/src/main/java/org/iotcity/iot/framework/actor/config/ApplicationConfigPool.java
+++ iot-framework-actor/src/main/java/org/iotcity/iot/framework/actor/config/ApplicationConfigPool.java
@@ -1,5 +1,10 @@
 package org.iotcity.iot.framework.actor.config;
 
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+import org.iotcity.iot.framework.core.util.task.PriorityLimitedBlockingQueue;
+
 /**
  * Thread pool executor configure data of application.<br/>
  * This configuration will be used for asynchronous callback response task processing and actor procedure processing.
@@ -8,19 +13,19 @@ package org.iotcity.iot.framework.actor.config;
 public final class ApplicationConfigPool {
 
 	/**
-	 * The number of threads to keep in the pool (0 by default).
+	 * The number of threads to keep in the pool (8 by default).
 	 */
-	public int corePoolSize = 0;
+	public int corePoolSize = 8;
 	/**
-	 * The maximum number of threads to allow in the pool (10 by default).
+	 * The maximum number of threads to allow in the pool (8 by default).
 	 */
-	public int maximumPoolSize = 10;
+	public int maximumPoolSize = 8;
 	/**
 	 * The maximum seconds that excess idle threads will wait for new tasks before terminating (60s by default).
 	 */
 	public long keepAliveTime = 60;
 	/**
-	 * The capacity of blocking queue to cache tasks when reaches the maximum of threads in the pool (1000 by default).
+	 * The capacity of blocking queue to cache tasks in thread pool executor (1000 by default, when set to 0, the synchronous queue {@link SynchronousQueue } is used; when set to greater than 0, the bounded priority blocking queue {@link PriorityLimitedBlockingQueue } is used; when set to less than 0, the unbounded priority blocking queue {@link PriorityBlockingQueue } is used).
 	 */
 	public int capacity = 1000;
 
