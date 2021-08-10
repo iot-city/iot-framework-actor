4b5368caba723804c9d9382ce7a168a4a9f4b68c
ardon <ardon@iot-city.org>
2021-08-10 21:46:45 +0800

    Add actor configuration file.

--- iot-framework-actor/src/main/java/org/iotcity/iot/framework/actor/config/ApplicationConfigPool.java
+++ iot-framework-actor/src/main/java/org/iotcity/iot/framework/actor/config/ApplicationConfigPool.java
@@ -17,16 +17,16 @@ public final class ApplicationConfigPool {
 	 */
 	public int corePoolSize = 8;
 	/**
-	 * The maximum number of threads to allow in the pool (8 by default).
+	 * The maximum number of threads to allow in the pool (16 by default).
 	 */
-	public int maximumPoolSize = 8;
+	public int maximumPoolSize = 16;
 	/**
 	 * The maximum seconds that excess idle threads will wait for new tasks before terminating (60s by default).
 	 */
 	public long keepAliveTime = 60;
 	/**
-	 * The capacity of blocking queue to cache tasks in thread pool executor (1000 by default, when set to 0, the synchronous queue {@link SynchronousQueue } is used; when set to greater than 0, the bounded priority blocking queue {@link PriorityLimitedBlockingQueue } is used; when set to less than 0, the unbounded priority blocking queue {@link PriorityBlockingQueue } is used).
+	 * The capacity of blocking queue to cache tasks in thread pool executor (8000 by default, when set to 0, the synchronous queue {@link SynchronousQueue } is used; when set to greater than 0, the bounded priority blocking queue {@link PriorityLimitedBlockingQueue } is used; when set to less than 0, the unbounded priority blocking queue {@link PriorityBlockingQueue } is used).
 	 */
-	public int capacity = 1000;
+	public int capacity = 8000;
 
 }
